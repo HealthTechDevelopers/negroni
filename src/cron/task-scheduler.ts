2ab95bdf6fff3e63cd05eaaf6f3d29677e58c2bf
Kacper Perzankowski <perzankowski.kacper@gmail.com>
2020-02-14 15:38:22 +0100

    add logs

--- src/cron/task-scheduler.ts
+++ src/cron/task-scheduler.ts
@@ -38,12 +38,12 @@ export class TaskScheduler {
           if (event.status !== 'cancelled' && Array.isArray(event.attendees)) {
             const userAsAttendeee = event.attendees.find((x) => x.email === user.email)
 
+            console.log(event);
             if (
               userAsAttendeee &&
               userAsAttendeee.responseStatus !== 'declined' &&
               moment().diff(moment(event.end.dateTime), 'minutes') > 5
             ) {
-              console.log(event);
               this.logger.log(`creating survey for ended event '${event.summary}' (${event.start.dateTime} - ${event.end.dateTime}), userId: ${user.id}, userEmail: ${user.email}`);
               this.surveyService.createSurvey(
                 user.id,
